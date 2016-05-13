c0a18e169523484e252e6110c212454e4ca78f6e
Ansi6622 <HotFuzz1*>
2016-05-12 18:18:26 -0600

    Created a simple data object nested in a parent object

--- src/Home/home_controller.js
+++ src/Home/home_controller.js
@@ -4,6 +4,11 @@ export default class HomeController{
     const vm = this;
 
     vm.message = 'Hi.'
-    
+    vm.theReview = [
+       {taste: "Poor. We didnt like the behaviour of our duck", author: "John-e boiiii", reputation: "Notoriously underwhelming", status: {
+         alive: true, 
+         arranged: "Less than great",
+         donor: true}
+     }]
   }
 }
